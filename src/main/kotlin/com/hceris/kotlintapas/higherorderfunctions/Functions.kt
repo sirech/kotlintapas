14ea221d6e6ec922365b1e6c756fce219aa271ca
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-09-29 19:57:46 +0200

    solution for higher order functions tapa

--- src/main/kotlin/com/hceris/kotlintapas/higherorderfunctions/Functions.kt
+++ src/main/kotlin/com/hceris/kotlintapas/higherorderfunctions/Functions.kt
@@ -1,3 +1,9 @@
 package com.hceris.kotlintapas.higherorderfunctions
 
-fun <T, U> Collection<T>.ourMap(transform: (T) -> U) = this
+fun <T, U> Collection<T>.ourMap(transform: (T) -> U): Collection<U> {
+    return arrayListOf<U>().also { newList ->
+        for (item in this) {
+            newList.add(transform(item))
+        }
+    }
+}
