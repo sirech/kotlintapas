6c92f7d9efac4e6f373d651fd34391d317ef40c4
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-09-29 21:38:46 +0200

    solution for destructuring tapa

--- src/main/kotlin/com/hceris/kotlintapas/destructuring/Destructuring.kt
+++ src/main/kotlin/com/hceris/kotlintapas/destructuring/Destructuring.kt
@@ -1,9 +1,11 @@
 package com.hceris.kotlintapas.destructuring
 
-class Point(val x: Int, val y: Int)
+class Point(val x: Int, val y: Int) {
+    operator fun component1() = x
+    operator fun component2() = y
+}
 
 fun asString(p: Point): String {
-    val x = p.x
-    val y = p.y
+    val (x, y) = p
     return "Point[$x, $y]"
 }
