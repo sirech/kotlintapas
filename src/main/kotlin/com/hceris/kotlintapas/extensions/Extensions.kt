b33e1bace4dbf15e77fac5aacec28535fa64227f
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-10-02 17:49:22 +0200

    solution for extensions tapa

--- src/main/kotlin/com/hceris/kotlintapas/extensions/Extensions.kt
+++ src/main/kotlin/com/hceris/kotlintapas/extensions/Extensions.kt
@@ -1,10 +1,10 @@
 package com.hceris.kotlintapas.extensions
 
-fun isAnagram(s: String, s2: String): Boolean {
-    return s.toList().sorted() == s2.toList().sorted()
+fun String.isAnagram(s: String): Boolean {
+    return s.toList().sorted() == this.toList().sorted()
 }
 
-fun isTheAnswerToEverything(n: Int) = n == 42
+fun Int.isTheAnswerToEverything() = this == 42
 
-fun isCool(list: List<String>) = "cool" in list
+fun List<String>.isCool() = "cool" in this
 
