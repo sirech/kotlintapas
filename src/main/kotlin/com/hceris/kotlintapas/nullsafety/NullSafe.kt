f95d3b4558e621f8ec01a89c2e276a5c1387386f
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-10-04 11:06:20 +0200

    solution for nullsafety tapa

--- src/main/kotlin/com/hceris/kotlintapas/nullsafety/NullSafe.kt
+++ src/main/kotlin/com/hceris/kotlintapas/nullsafety/NullSafe.kt
@@ -3,10 +3,7 @@ package com.hceris.kotlintapas.nullsafety
 fun Map<String, String>.hidePassword(): Map<String, String> {
     return toMutableMap().also { copy ->
         // String? could be null, we are forced to declare what to do
-        val password = copy["password"]
-
-        if (password != null) {
-            // This function always returns a String, nothing needs to be handled
+        copy["password"]?.let { password ->
             copy["password"] = password.hidden()
         }
     }.toMap()
